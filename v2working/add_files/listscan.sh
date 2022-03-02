ae41319bb195f226e06c5fb6bb63cb73226db193
dreamerRAJ <rajnishkr5060@gmail.com>
2022-03-03 00:38:19 +0530

    v2_development

--- v2working/add_files/listscan.sh
+++ v2working/add_files/listscan.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/bash
 
-figlet -f amc3line 'LIST SCAN WITH NMAP' | lolcat -a -d 2 -F 0.5
+bold=$(tput bold)
+normal=$(tput sgr0)
 
+
+figlet -f amc3line 'LIST SCAN WITH NMAP' | lolcat -a -d 2 -F 0.5
+sleep 1
+read -p "${bold}|> Enter Your IP's List Path : ${normal}" ip_addr 
+sleep 1
+while read ip; do
+echo -e "\e[101m [+] Testing IP : $ip\e[0m" && nmap -v -sV -d -A -p1-65535 --script vuln,default -T4 -Pn $ip
+done < $ip_addr
